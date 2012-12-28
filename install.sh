efa3db8ca62769e82677ec1c7374a74cc49b8263
Sam Kimbrel <sam@snapguide.com>
2012-12-28 14:54:19 -0800

    Oops, make the bundle directory point to the right place

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ ln -s ~/dotfiles/bash_helpers ~/.bash_helpers
 ln -s ~/dotfiles/git-completion.sh ~/.git-completion.sh
 ln -s ~/dotfiles/bash_prompt ~/.bash_prompt
 ln -s ~/dotfiles/bashrc ~/.bashrc
-mkdir -p ~/.vim/bundle ~/.vim/autoload
+mkdir -p ~/.vim/autoload
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+ln -s ~/dotfiles/bundle ~/.vim/bundle
 git submodule update --init
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/ackrc ~/.ackrc
